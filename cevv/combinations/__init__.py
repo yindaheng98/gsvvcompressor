142064e994c4fdf7ebd5b5b0ee7f0c00b94d4d50
yindaheng98 <yindaheng98@gmail.com>
2026-01-26 20:54:02 -0800

    collect

--- cevv/combinations/__init__.py
+++ cevv/combinations/__init__.py
@@ -4,7 +4,21 @@ Combination modules for composing multiple codec components.
 
 from .vq_xyz_zstd import create_vq_xyz_zstd_encoder, create_vq_xyz_zstd_decoder
 
+# Registry of encoder factory functions
+# Key: combination name, Value: encoder factory function
+ENCODERS = {
+    "vq_xyz_zstd": create_vq_xyz_zstd_encoder,
+}
+
+# Registry of decoder factory functions
+# Key: combination name, Value: decoder factory function
+DECODERS = {
+    "vq_xyz_zstd": create_vq_xyz_zstd_decoder,
+}
+
 __all__ = [
     "create_vq_xyz_zstd_encoder",
     "create_vq_xyz_zstd_decoder",
+    "ENCODERS",
+    "DECODERS",
 ]
